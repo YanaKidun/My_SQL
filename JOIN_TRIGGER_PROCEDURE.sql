186be9bf7938beb106279faad0302b686d16baf5
YanaKidun <stabrovskayaya@gmail.com>
2022-04-28 20:17:04 +0300

    Add files with examples

---
+++ JOIN_TRIGGER_PROCEDURE.sql
@@ -0,0 +1,287 @@
+/* Урок № 10. Транзакции, переменные, представления. Администрирование. 
+ * 			  Хранимые процедуры и функции, триггеры.
+* Задание 1. Написать процедуру для рекомендации пользователю новых друзей (живут в одном городе, состоят в одном сообществе)
+*/
+
+DROP PROCEDURE IF EXISTS sp_friendship_recommendations;
+-- ставим новое обозначение окончания команды (разделитель)
+DELIMITER //
+CREATE PROCEDURE sp_frs(IN for_user_id BIGINT UNSIGNED)
+BEGIN
+	SELECT for_user_id ;
+END//
+
+CALL sp_frs(2);
+
+
+
+DELIMITER //
+CREATE PROCEDURE sp_friendship_recommendations(IN for_user_id BIGINT UNSIGNED)
+BEGIN
+	SELECT p2.user_id 
+	FROM profiles p1 -- из одного города
+	JOIN profiles p2 
+	ON p1.city = p2.city  -- выбираем пользователей p2 из того же города, что и заданный пользователь
+	WHERE p1.user_id = for_user_id -- находим в p1 город заданного пользователя
+	AND p2.user_id != for_user_id -- исключаем заданного пользователя из p2   
+	UNION -- объединяем два найденных сета
+	SELECT cu2.user_id 
+	FROM communities_users cu1 -- из одного сообщества
+	JOIN communities_users cu2 
+	ON cu1.community_id = cu2.community_id -- выбираем пользователей uc2 из тех же сообществ, что и заданный пользователь
+	WHERE cu1.user_id = for_user_id  -- находим в uc1 сообщества заданного пользователя
+	AND cu2.user_id != for_user_id -- исключаем заданного пользователя из uc2
+	ORDER BY RAND() -- берем случайные записи
+	LIMIT 5; -- выводим только 5 человек из рекомендаций
+END// -- обозначаем конец процедуры новым разделителем
+
+-- возвращаем стандартный разделитель
+DELIMITER ;
+
+-- вызываем функцию для пользователя 2
+CALL sp_friendship_recommendations(2);
+
+/*
+ * Задание 2. Посчитать популярность пользователя как отношения заявок в друзья.
+ * 			  популярность = (кол-во входящих заявок)/(кол-во исходящих заявок)
+*/
+
+DROP FUNCTION IF EXISTS func_user_popularity;
+
+DELIMITER // -- выставим разделитель
+CREATE FUNCTION func_user_popularity(for_user_id BIGINT UNSIGNED)
+RETURNS FLOAT READS SQL DATA -- так как мы только читаем данные
+BEGIN 
+	DECLARE cnt_to_user INT;
+	DECLARE cnt_from_user INT;
+	
+	-- посчитаем количество входящих заявок
+	SET cnt_to_user = (SELECT COUNT(*) FROM friend_requests WHERE to_user_id = for_user_id);
+
+	-- посчитаем количество исходящих заявок другим способом
+	SELECT COUNT(*) INTO cnt_from_user FROM friend_requests WHERE from_user_id = for_user_id;
+
+    -- хотим выводить результат даже при отсутсвии исходящих заявок
+
+	IF cnt_from_user = 0
+	THEN 
+		RETURN cnt_to_user;
+	ELSE
+		RETURN cnt_to_user / cnt_from_user;
+	END IF;
+END//
+
+DELIMITER ; -- возвращаем исходный разделитель
+
+-- вызов функции
+SELECT TRUNCATE(func_user_popularity(1), 1);
+
+SELECT id, firstname, lastname, func_user_popularity(id) AS popularity 
+FROM users
+ORDER BY func_user_popularity(id) DESC;
+
+-- Использование функции: выведем друзей пользователя 1, отранжировав их по популярности
+SELECT DISTINCT u.id 
+FROM friend_requests fr 
+JOIN users u 
+ON fr.from_user_id = u.id OR fr.to_user_id = u.id 
+WHERE (fr.from_user_id = 1 OR fr.to_user_id = 1) 
+AND fr.request_type = 1 
+AND u.id != 1
+ORDER BY func_user_popularity(u.id) DESC;
+
+/*
+ * Задание 3. Добавим нового пользователя через транзакцию.
+*/
+
+START TRANSACTION; -- начало транзакции
+
+INSERT INTO users (firstname, lastname, email, phone)
+VALUES ('Transaction', 'User', 'new@mail.com', 89111234567);
+
+INSERT INTO profiles (user_id, gender, birthday, city, country)
+VALUES (last_insert_id(), 'm', '1999-10-10', 'Moscow', 'Russia');
+
+-- ROLLBACK; -- отмена транзакции, новых строк добавлено не будет
+
+COMMIT; -- успешное завершение транзакции, были добавлены строки в users и profiles
+
+/*
+ * Задание 4. Добавим нового пользователя через процедуру и транзакцию.
+*/
+DROP PROCEDURE IF EXISTS sp_add_user;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_add_user(firstname VARCHAR(145), lastname VARCHAR(145), email VARCHAR(145), phone CHAR(11),
+							gender CHAR(1), birthday DATE, city VARCHAR(130), country VARCHAR(130), OUT tran_result VARCHAR(200))
+BEGIN 
+	DECLARE tran_rollback BOOL DEFAULT 0;
+	DECLARE code VARCHAR(100);
+	DECLARE error_string VARCHAR(100);
+
+	
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION  -- обработка ошибок
+	BEGIN 
+		SET tran_rollback = 1; -- если пойман exception ставим флажок
+		GET stacked DIAGNOSTICS CONDITION 1
+			code = RETURNED_SQLSTATE, error_string = MESSAGE_TEXT; -- выбираем код ошибки и сообщение
+		SET tran_result := CONCAT(code, ': ', error_string); -- собираем в результат
+	END;
+
+	START TRANSACTION;  -- запускаем транзакцию добавления пользователя
+	
+	INSERT INTO users (firstname, lastname, email, phone)
+	VALUES (firstname, lastname, email, phone);
+
+	INSERT INTO profiles (user_id, gender, birthday, city, country)
+	VALUES (last_insert_id(), gender, birthday, city, country);
+
+	IF tran_rollback  -- если была получена ошибка откатываем изменения
+	THEN 
+		ROLLBACK;
+	ELSE
+		SET tran_result := 'ok'; -- если пользователь добавлен, выводим ок
+		COMMIT; -- успешно завершаем транзакцию
+	END IF;
+END//
+
+DELIMITER ;
+
+-- вызываем процедуру
+CALL sp_add_user('Transaction2', 'User2', 'new2@mail.com', 89303099090, 
+				 'm', '1990-11-11',  'Moscow', 'Russia', @tran_result);
+-- смотрим отладочную информацию		
+SELECT @tran_result;
+
+CALL sp_add_user('Transaction3', 'User3', 'new3@mail.com', 83303099090, 
+				 'm', '1990-11-11',  'Moscow', 'Russia', @tran_result);
+				
+SELECT @tran_result;
+
+/*
+ * Задание 5. Представление для вывода всех друзей пользователя.
+*/
+
+-- представление, выбирающее друзей пользователей
+CREATE OR REPLACE VIEW view_friends AS
+SELECT u.id, u1.id AS friend_id, u1.firstname, u1.lastname 
+FROM friend_requests fr 
+JOIN users u 
+ON (fr.from_user_id = u.id OR fr.to_user_id = u.id)
+JOIN users u1 
+ON (fr.from_user_id = u1.id OR fr.to_user_id = u1.id)
+WHERE u.id != u1.id 
+ORDER BY u.id;
+
+ -- смотрим на содержимое представления
+SELECT * FROM view_friends;
+
+ -- список друзе пользователя 1
+SELECT friend_id, firstname, lastname 
+FROM view_friends 
+WHERE id = 1;
+
+-- удаляем представление
+-- DROP VIEW view_friends;
+
+/*
+ * Задание 6. Вывести рекомендации друзей для пользователя без его друзей.
+*/
+DROP PROCEDURE IF EXISTS sp_friendship_recommendations_advanced;
+
+DELIMITER //
+
+CREATE PROCEDURE sp_friendship_recommendations_advanced(IN from_user_id BIGINT UNSIGNED)
+BEGIN 
+	SELECT * FROM 
+		(SELECT p2.user_id FROM profiles p1
+		JOIN profiles p2 ON p1.city = p2.city 
+		WHERE p1.user_id = from_user_id AND p2.user_id != from_user_id
+		UNION
+		SELECT cu2.user_id FROM communities_users cu1
+		JOIN communities_users cu2 ON cu1.community_id = cu2.community_id
+		WHERE cu1.user_id = from_user_id AND cu2.user_id != from_user_id) AS t_recommendations
+	WHERE t_recommendations.user_id NOT IN (SELECT friend_id FROM view_friends WHERE id = from_user_id) 
+	ORDER BY RAND()
+	LIMIT 5;	
+END//
+
+DELIMITER ;
+
+CALL sp_friendship_recommendations_advanced(10); 
+
+/*
+ * Задание 7. Создать триггер для контроля даты рождения пользователя.
+*/
+
+DELIMITER //
+
+CREATE TRIGGER check_birthday_before_insert BEFORE INSERT ON profiles 
+FOR EACH ROW 
+BEGIN 
+	IF NEW.birthday >= CURRENT_DATE() THEN 
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Insert Canceled. Birthday must be in the past!';
+	END IF;
+END//
+
+DELIMITER ;
+
+-- неудача с добавлением пользователя с днем рождения больше текущей даты
+INSERT INTO users (firstname, lastname, email, phone) VALUES 
+	('Trigger', 'Check', 'trigger@mail.com', '89111111111');
+	
+INSERT INTO profiles (user_id, gender, birthday) VALUES 
+	(last_insert_id(), 'x', '2030-12-01');
+
+DELIMITER //
+
+CREATE TRIGGER check_birthday_before_update BEFORE UPDATE ON profiles 
+FOR EACH ROW 
+BEGIN 
+	IF NEW.birthday >= current_date() THEN 
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Update Canceled. Birthday must be in the past!';
+	END IF;
+END//
+
+DELIMITER ;
+
+-- неудача с обновлением даты рождения на больше текущей 
+UPDATE profiles SET birthday = '2030-01-01' WHERE user_id = 1;
+
+
+SELECT o.book, b.name,g.name  FROM orders o 
+JOIN   books b 
+ON o.book =b.id
+JOIN   genres g
+ON b.genre=g.id
+WHERE o.from_user =5
+
+SELECT b.name FROM books b
+WHERE b.genre = (SELECT DISTINCT g.id  FROM orders o 
+JOIN   books b 
+ON o.book =b.id
+JOIN   genres g
+ON b.genre=g.id
+WHERE o.from_user =5)
+ORDER BY RAND() 
+	LIMIT 2;
+
+SELECT DISTINCT  g.id  FROM orders o 
+JOIN   books b 
+ON o.book =b.id
+JOIN   genres g
+ON b.genre=g.id
+WHERE o.from_user =5;
+
+SELECT p2.user_id FROM profiles p1 -- из одного города
+	JOIN profiles p2 ON p1.city = p2.city  -- выбираем пользователей p2 из того же города, что и заданный пользователь
+	WHERE p1.user_id = for_user_id -- находим в p1 город заданного пользователя
+	AND p2.user_id != for_user_id -- исключаем заданного пользователя из p2   
+	UNION -- объединяем два найденных сета
+	SELECT cu2.user_id FROM communities_users cu1 -- из одного сообщества
+	JOIN communities_users cu2 ON cu1.community_id = cu2.community_id -- выбираем пользователей uc2 из тех же сообществ, что и заданный пользователь
+	WHERE cu1.user_id = for_user_id  -- находим в uc1 сообщества заданного пользователя
+	AND cu2.user_id != for_user_id -- исключаем заданного пользователя из uc2
+	ORDER BY RAND() -- берем случайные записи
+	LIMIT 3; -- выводим только 5 человек из рекомендаций
---
+++ VIEW_TRANSACTION.sql
@@ -0,0 +1,274 @@
+
+DROP TABLE IF EXISTS accounts;
+CREATE TABLE accounts (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  total DECIMAL (11,2) COMMENT 'Счет',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Счета пользователей и интернет магазина';
+
+INSERT INTO accounts (user_id, total) VALUES
+  (4, 5000.00),
+  (3, 0.00),
+  (2, 200.00),
+  (NULL, 25000.00);
+ SELECT * FROM accounts;
+ 
+START TRANSACTION;
+SELECT total FROM accounts WHERE user_id = 4;
+UPDATE accounts SET total = total - 2000 WHERE user_id = 4;
+UPDATE accounts SET total = total + 2000 WHERE user_id IS NULL;
+SELECT * FROM accounts;
+COMMIT;
+
+START TRANSACTION;
+SELECT total FROM accounts WHERE user_id = 4;
+UPDATE accounts SET total = total - 2000 WHERE user_id = 4;
+UPDATE accounts SET total = total + 2000 WHERE user_id IS NULL;
+ROLLBACK;
+
+
+START TRANSACTION;
+SELECT total FROM accounts WHERE user_id = 4;
+SAVEPOINT accounts_4;
+UPDATE accounts SET total = total - 2000 WHERE user_id = 4;
+ROLLBACK TO SAVEPOINT accounts_4;
+
+SELECT @total := COUNT(*) FROM products;
+SELECT @total;
+SELECT @price := MAX(price) FROM products;
+SELECT * FROM products WHERE price = @price;
+
+SELECT @id := id FROM products;
+SELECT @id;
+
+SELECT @id := 5, @ID := 3;
+SELECT @id, @ID;
+
+SET @last = NOW() - INTERVAL 7 DAY;
+SELECT CURDATE(), @last;
+
+SELECT * FROM tbl1;
+SET @start := 0;
+SELECT @start := @start + 1 AS id, value FROM tbl1;
+
+CREATE TEMPORARY TABLE temp (id INT, name VARCHAR(255));
+SHOW TABLES;
+DESCRIBE temp;
+
+PREPARE ver FROM 'SELECT VERSION()';
+EXECUTE ver;
+
+PREPARE prd FROM 'SELECT id, name, price FROM products WHERE catalog_id = ?'; /*даним запрос только одного из разделов*/
+SET @catalog_id = 1;
+EXECUTE prd USING @catalog_id;
+
+DROP PREPARE prd;/* удалить длинамическую запрос*/
+
+
+
+/*представления*/
+SELECT * FROM catalogs;
+CREATE VIEW cat AS SELECT * FROM catalogs ORDER BY name;
+SELECT * FROM cat;
+SHOW TABLES;
+
+CREATE VIEW cat_reverse (catalog, catalog_id)
+AS SELECT name, id FROM catalogs;
+SELECT * FROM cat_reverse;
+
+CREATE OR REPLACE VIEW namecat (id, name, total)
+AS SELECT id, name, LENGTH(name) FROM catalogs;
+SELECT * FROM namecat ORDER BY total DESC;
+
+CREATE ALGORITHM = TEMPTABLE VIEW cat2 AS SELECT * FROM catalogs;
+
+CREATE ALGORITHM = TEMPTABLE VIEW cat2 AS SELECT * FROM catalogs;
+DESCRIBE products;
+
+CREATE OR REPLACE VIEW prod AS
+SELECT id, name, price, catalog_id
+FROM products
+ORDER BY catalog_id, name;
+
+SELECT * FROM prod;
+
+SELECT * FROM prod ORDER BY name DESC;
+
+CREATE OR REPLACE VIEW processors AS
+SELECT id, name, price, catalog_id
+FROM products
+WHERE catalog_id = 1;
+
+SELECT * FROM processors;
+
+CREATE VIEW v1 AS
+SELECT * FROM tbl1 WHERE value < 'fst5'
+WITH CHECK OPTION;
+
+INSERT INTO v1 VALUES ('fst4');
+INSERT INTO v1 VALUES ('fst5');
+ALTER VIEW v1 AS
+SELECT * FROM tbl1 WHERE value > 'fst4'
+WITH CHECK OPTION;
+CREATE OR REPLACE VIEW v1 AS
+SELECT * FROM tbl1 WHERE value > 'fst4'
+WITH CHECK OPTION;
+DROP VIEW cat, cat_reverse, namecat, prod, processors, v1;
+DROP VIEW IF EXISTS cat, cat_reverse, namecat, prod, processors, v1;
+
+
+
+/*В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных. 
+ * Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.*/
+
+
+SELECT *FROM shop.users; 
+SELECT *FROM sample.users;
+
+SELECT id,lastname
+FROM shop.users;
+
+start TRANSACTION;
+INSERT INTO sample.users
+SELECT shop.users.id, shop.users.firstname FROM shop.users
+WHERE id=11;
+/*DELETE FROM shop.users /*не удаляет из-за ключа
+WHERE id=11 LIMIT 1;*/
+COMMIT;
+
+/*Создайте представление, которое выводит название name товарной позиции из таблицы products
+ *  и соответствующее название каталога name из таблицы catalogs.*/
+
+SELECT * FROM catalogs;
+
+CREATE VIEW task3 (name, name1)
+AS SELECT c.name, p.name  
+FROM catalogs c, products p 
+ORDER BY c.name;
+SELECT * FROM task3;
+
+
+SELECT p.name AS product_name, c.name AS catalog_name 
+FROM products p
+JOIN catalogs c;
+
+
+/*Пусть имеется таблица с календарным полем created_at. 
+ В ней размещены разряженые календарные записи за август 2018 года '2018-08-01', '2016-08-04', '2018-08-16' 
+ и 2018-08-17. Составьте запрос, который выводит полный список дат за август, 
+ выставляя в соседнем поле значение 1, если дата присутствует в исходном таблице и 0, если она отсутствует.*/
+
+SELECT *
+FROM orders o ;
+
+CREATE TEMPORARY TABLE last_days_feb(
+DAY int);
+
+INSERT INTO last_days_feb VALUES
+(0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),
+(14),(15),(16),(17),(18),(19),(20),(21),(22),(23),(24),(25),(26),(27),(28);
+
+SELECT * FROM last_days_feb;
+
+SELECT 
+date (date('2022-02-28') - INTERVAL l.day DAY )AS Days
+FROM 
+last_days_feb l
+ORDER BY DAY desc;
+
+SELECT o.id AS orderss,
+date (date('2022-02-28') - INTERVAL l.day DAY )AS Days
+FROM 
+last_days_feb l
+LEFT JOIN 
+orders o
+ON (date('2022-02-28') - INTERVAL l.day DAY ) = o.created_at
+ORDER BY o.created_at ;
+
+
+
+/*Пусть имеется любая таблица с календарным полем created_at. 
+Создайте запрос, который удаляет устаревшие записи из таблицы, оставляя только 5 самых свежих записей.*/
+/* смысл такой ввести переменную, которая покажет максимальное кол-во строки(последнюю строку) от нее и будем отталкиваться,
+ * если нам надо увюрать 5 последних или 5 первых записей - сделаит сотритировк и удалить или вывести необходимые данные*/
+
+SELECT *
+FROM orders o ;
+
+SELECT @total := COUNT(*) FROM orders o ;
+SELECT *
+FROM orders o 
+WHERE id>=@total-2
+ORDER BY created_at ASC ;
+
+
+/*Создайте хранимую функцию hello(), которая будет возвращать приветствие, 
+ * в зависимости от текущего времени суток. С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", 
+ * с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", с 18:00 до 00:00 — "Добрый вечер", 
+ * с 00:00 до 6:00 — "Доброй ночи".*/
+
+SELECT now(), HOUR (now());
+
+delimiter //
+SELECT now(), HOUR (now())//
+
+CREATE FUNCTION get_hour()
+RETURNS int NOT DETERMINISTIC 
+BEGIN
+	RETURN HOUR (now());
+end 
+
+CREATE FUNCTION hello ()
+RETURNS tinytext NOT DETERMINISTIC
+BEGIN
+	DECLARE HOUR int 
+	SET HOUR = HOUR (now());
+	CASE
+	WHEN HOUR BETWEEN 0 AND 5
+		THEN 
+		RETURN "good night"
+	WHEN HOUR BETWEEN 6 AND 11
+		THEN 
+		RETURN "good morning"
+	WHEN HOUR BETWEEN 12 AND 17
+		THEN 
+		RETURN "good day"
+	WHEN HOUR BETWEEN 17 AND 23
+		THEN 
+		RETURN "good evening"
+END CASE;
+END//
+
+
+/*В таблице products есть два текстовых поля: name с названием товара и description с его описанием. 
+ Допустимо присутствие обоих полей или одно из них. Ситуация, когда оба поля принимают неопределенное 
+ значение NULL неприемлема. Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля 
+ были заполнены. При попытке присвоить полям NULL-значение необходимо отменить операцию.*/
+
+CREATE TRIGGER validate_name_description_insert BEFORE INSERT ON products 
+FOR EACH ROW BEGIN 
+	IF NEW.name IS NULL AND NEW.description IS NULL THEN 
+	SIGNAL SQLSTATE '45000'
+	SET message_text = 'name and description is null';
+END if;
+END
+
+INSERT INTO products 
+(name, description, price, catalog_id)
+VALUES
+(NULL, NULL, 93600.00, 2);
+
+CREATE TRIGGER validate_name_description_update BEFORE UPDATE ON products 
+FOR EACH ROW BEGIN 
+	IF NEW.name IS NULL AND NEW.description IS NULL THEN 
+	SIGNAL SQLSTATE '45000'
+	SET message_text = 'name and description is null';
+END if;
+END
+
+
+
+
+
